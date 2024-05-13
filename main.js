f444250540106c6db8f5addd0309646af0d87d5e
kawgatinhadozap123 <110419527+kawgatinhadozap123@users.noreply.github.com>
2024-05-13 09:57:17 -0300

    .

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-04-02T00:00:00");
 const tempoObjetivo2 = new Date("2024-07-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo3 = new Date("2024-10-31T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-24T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
